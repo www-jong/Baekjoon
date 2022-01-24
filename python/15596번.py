093667bc2055c11860b7fc9f21109edbd0b40224
Wjong <88592432+www-jong@users.noreply.github.com>
2022-01-25 02:04:37 +0900

    dd

---
+++ python/15596번.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.6
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def solve(a:list):
+    return int(sum(a))
+
