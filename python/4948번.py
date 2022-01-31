1eb89dfc1c8453759ec02bd14bef2459fe593412
놋북 <weon1009@gmail.com>
2022-01-31 19:19:00 +0900

    0131 공부

---
+++ python/4948번.py
@@ -0,0 +1,42 @@
+"""
+1차시도, 되긴되지만 시간초과
+while True:
+    n=int(input())
+    if n ==0:
+        break
+    co=0
+    for i in range(n+1,n*2+1):
+        ch=0
+        for j in range(2,i//2+1):
+            if i%j==0:
+                ch=1
+                break
+        if ch==0 and i!=1:
+            co+=1
+    print(co)
+"""
+while True:
+    n=int(input())
+    if n==0:
+        break
+    arr=[1 for i in range(n*2+1)]
+    for i in range(n+1,n*2+1):
+        ch=0
+        if arr[i]==0:
+            continue
+        elif arr[i]==1:
+            for j in range(2,int(i**0.5)+1):
+                if i%j==0:
+                    ch=1
+                    f=i
+                    while f<=n*2:
+                        arr[f]=0
+                        f+=f
+                    break
+    count=0
+    for i in range(n+1,n*2+1):
+        if arr[i]==1 and i!=1:
+            count+=1
+    print(count)
+            
+    
