f9f9fe161f77f9ee7ff8bd8d9fe95d67f223a294
Wjong <88592432+www-jong@users.noreply.github.com>
2022-02-17 23:51:50 +0900

    dd

--- python/15650번.py
+++ python/15650번.py
@@ -1,5 +1,20 @@
 n,m=map(int,input().split())
-s=0
+arr=[]
 
 def func():
+    if len(arr)==m:
+        print(*arr,sep=" ")
+        return
+    for i in range(1,n+1):
+        if i not in arr:
+            if len(arr)>=1 and i > arr[-1]:
+                arr.append(i)
+                func()
+                arr.pop()
+            elif len(arr)==0:
+                arr.append(i)
+                func()
+                arr.pop()
+
+func()
     
