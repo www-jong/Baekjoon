379b290c648fe3a1f228c698290e1b37110e5bcf
Wjong <weon1009@gmail.com>
2022-04-01 17:45:21 +0900

    04012

--- 1259번.py
+++
@@ -1,9 +0,0 @@
-while True:
-    c=input()
-    if c=="0":
-        break
-    for i in range(len(c)//2):
-        if c[i]!=c[len(c)-1-i]:
-            print("no")
-        else:
-            print("yes")
