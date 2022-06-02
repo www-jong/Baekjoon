6e35a9af6d393637cf7e53b528fe64869c5fbc4e
www-jong <weon1009@gmail.com>
2022-06-02 23:05:01 +0900

    0602

--- python/2225번_3.py
+++ python/2225번_3.py
@@ -10,11 +10,11 @@ for i in range(2,n+1):
     for j in range(2,k+1):
         dp[i][j]=dp[i-1][j]+dp[i][j-1]
 print(dp[n][k]%1000000000)
-print(dp)
 '''
 dp[i][k]= 정수k개를 더해 i가 되는 경우의수
 dp[i][x]=dp[i][x-1]에서0을 더한방법+dp[i-1][x]에서...
 아오
+
 
 k=  1  2  3  4  5
 0   1  1  1  1  1
