250a9cea8e6f1ecc2958bc2366645bdc6a7afd72
www-jong <weon1009@gmail.com>
2022-06-10 20:07:46 +0900

    0610

--- python/2206번_2.py
+++ python/2206번_2.py
@@ -9,31 +9,31 @@ for i in range(n):
         li.append(int(j))
     graph.append(li)
 dx,dy=[-1,1,0,0],[0,0,-1,1]
-visit=[[0]*(m+1) for i in range(n+1)]
-movelist=[]
+visit=[[[0,0] for i in range(m+1)] for i in range(n+1)]
+ans=100000000
 def bfs(graph,visit,node,q):
-    global movelist
+    global ans
     q.append((node[0],node[1],node[2]))
-    visit[node[0]][node[1]]=1
+    visit[node[0]][node[1]][node[2]]=1
     while q:
         x,y,st=q.popleft()
-        wells=[]
         if x==n and y==m:
-            movelist.append(visit[x][y])
+            if visit[x][y][st]<ans:
+                ans=visit[x][y][st]
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
             if 1<=nx<=n and 1<=ny<=m:
-                if visit[nx][ny]==0 and graph[nx][ny]==0:
-                    visit[nx][ny]=visit[x][y]+1
+                if visit[nx][ny][st]==0 and graph[nx][ny]==0:
+                    visit[nx][ny][st]=visit[x][y][st]+1
                     q.append((nx,ny,st))
-                if visit[nx][ny]==0 and graph[nx][ny]==1 and st==0:
-                    visit[nx][ny]=visit[x][y]+1
+                if graph[nx][ny]==1 and st==0:
+                    visit[nx][ny][1]=visit[x][y][st]+1
                     q.append((nx,ny,1))
 q=deque()
 bfs(graph,visit,(1,1,0),q)
-if len(movelist)<1:
+if ans==100000000:
     print(-1)
 else:
-    print(min(movelist))
+    print(ans)
 
