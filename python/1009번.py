e8f509da44fbb58d04f72060bcdc5898eea3e0e9
Wjong <88592432+www-jong@users.noreply.github.com>
2022-06-11 23:25:11 +0900

    0611

--- python/1009번.py
+++ python/1009번.py
@@ -1,5 +1,8 @@
 import sys
+a=0
 for i in range(int(input())):
     a,b=map(int,sys.stdin.readline().split())
     ans=pow(a,b,10)
+
     sys.stdout.writelines(str(10 if ans==0 else ans)+"\n")
+    
