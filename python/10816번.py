16f99fc7d30f51c5f0b7353a1fde67cb51d3545c
www-jong <weon1009@gmail.com>
2022-06-14 15:21:38 +0900

    0614

--- python/10816번.py
+++ python/10816번.py
@@ -1,7 +1,17 @@
 import sys
 n=sys.stdin.readline()
-s=set(map(int,sys.stdin.readline().split()))
+s=list(map(int,sys.stdin.readline().split()))
+l={}
+for i in s:
+    if i in l:
+        l[i]+=1
+    else:
+        l[i]=1
 m=sys.stdin.readline()
-l=list(sys.stdin.readline())
-for i in range(len(l)):
-    l[i]=s.count(l[i])
+def adds(x):
+    if int(x) in l:
+        return l[int(x)]
+    else:
+        return 0
+l2=list(map(adds,sys.stdin.readline().split()))
+print(*l2,sep=' ')
