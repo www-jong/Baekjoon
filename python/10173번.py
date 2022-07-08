63485e9a251e45acbfb7095d4df6bc2b58cd3079
Wjong <88592432+www-jong@users.noreply.github.com>
2022-07-08 17:43:39 +0900

    0708

--- python/10173번.py
+++ python/10173번.py
@@ -1,17 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.6
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
 
 while True:
     arr=input()
