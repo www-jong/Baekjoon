357905ce0950f3341ad93ffd9b1f3c78bd34468c
Wjong <88592432+www-jong@users.noreply.github.com>
2022-07-13 23:41:08 +0900

    0713

--- python/test.py
+++ python/test.py
@@ -2,8 +2,3 @@ def get_cart_prd(pools):
   result = [[]]
   for pool in pools:
     result = [x+[y] for x in result for y in pool]
-  return result
-
-mylists = [['a', 'b'],
-    [1, 2, 3],[4,5,6]]
-print(get_cart_prd(mylists))
