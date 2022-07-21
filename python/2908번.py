398e8e8af54653cc63891847fba1b4cf6a622850
Wjong <88592432+www-jong@users.noreply.github.com>
2022-07-21 23:21:34 +0900

    0721

--- python/2908번.py
+++ python/2908번.py
@@ -1,20 +1,4 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.6
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
 
-# +
 a,b=map(int,input().split())
 a=list(reversed(list(str(a))))
 b=list(reversed(list(str(b))))
