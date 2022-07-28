afc875c11eadee5b9afbc445e9cc857f3e82b535
Wjong <weon1009@gmail.com>
2022-07-28 20:50:18 +0900

    0728

--- python/10830번.py
+++ python/10830번.py
@@ -1,26 +1,45 @@
-from copy import deepcopy
-n,b=map(int,input().split())
-li=[]
-tmpli=[[] for _ in range(n)]
-def mm(x):
-    return int(x)%1000
-def func(li1,li2):
-    tt=0
-    for i in range(n):
-        tt+=li1[i]*li2[i]
-    return tt%1000
+N,B=map(int,input().split())
+A=[]
+for i in range(N):
+    A.append(list(map(int,input().split())))
+print(A)
 
-for i in range(n):
-    li.append(list(map(mm,input().split())))
-for i in range(n):
-    for j in range(n):
-        tmpli[i].append(li[j][i])
-ans=deepcopy(li)
-for _ in range(b-1):
-    for i in range(n):
-        for j in range(n):
-            tmp=func(li[i],tmpli[j])
-            ans[i][j]=tmp
-    li=deepcopy(ans)
-for i in range(n):
+def func(li1,li2):
+    tmp=0
+    for i in range(N):
+        tmp+=li1[i]*li2[i]
+    return tmp%1000
+
+def multi(a,b):
+    tmp=[[] for _ in range(N)]
+    emp=[[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            tmp[i].append(b[j][i])
+
+    for i in range(N):
+        for j in range(N):
+            tem=func(a[i],tmp[j])
+            emp[i][j]=tem
+    return emp
+
+def sqrt(a,b):
+    if b==1:
+        for i in range(N):
+            for j in range(N):
+                a[i][j]%=1000
+        return a
+    elif b%2==0:
+        tmp=sqrt(a,b//2)
+        return multi(tmp,tmp)
+    else:
+        tmp=sqrt(a,b//2)
+        return multi(multi(tmp,tmp),a)
+ans=sqrt(A,B)
+for i in range(N):
     print(*ans[i])
+'''
+51   49
+25   
+'''
+    
