5ba330cd8c14a62a1dcd6a40116ab10490838952
www-jong <weon1009@gmail.com>
2022-09-26 18:08:29 +0900

    0926

--- python/24060번.py
+++ python/24060번.py
@@ -1,13 +1,6 @@
-n=int(input())
-def func1(n):
-    if n==1 or n==2:
-        return 1
-    else:
-        return (func1(n-1)+func1(n-2))
-def func2(n):
-    dp=[1]*(n+1)
-    for i in range(3,n+1):
-        dp[i]=dp[i-1]+dp[i-2]
-    return dp[n]
-for n in range(3,21):
-    print("%d %d %d"%(n,func2(n),n-2))
+N,K=map(int,input().split())
+li=list(map(int,input().split()))
+def merge_srt(li):
+    if li[0]<li[-1]:
+        mid=len(li)//2
+        merge_srt()
