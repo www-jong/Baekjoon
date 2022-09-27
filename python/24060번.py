583a45f9067f077f2491b47b42f98633cd3297d9
Wjong <weon1009@gmail.com>
2022-09-27 19:01:29 +0900

    0927

--- python/24060번.py
+++ python/24060번.py
@@ -1,6 +1,36 @@
 N,K=map(int,input().split())
 li=list(map(int,input().split()))
-def merge_srt(li):
-    if li[0]<li[-1]:
-        mid=len(li)//2
-        merge_srt()
+
+ans=[]
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    
+    mid = (len(arr)+1)//2
+   
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    i,j = 0,0
+    tmp = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            tmp.append(left[i])
+            ans.append(left[i])
+            i+=1
+        else:
+            tmp.append(right[j])
+            ans.append(right[j])
+            j+=1
+    while i < len(left):
+        tmp.append(left[i])
+        ans.append(left[i])
+        i+=1
+    while j < len(right):
+        tmp.append(right[j])
+        ans.append(right[j])
+        j+=1
+    
+    return tmp
+merge_sort(li)
+print(ans[K-1] if len(ans)>K else -1)
