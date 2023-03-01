05d219a6410cfa5b496b7080fa96fd23227b77b7
Wjong <weon1009@gmail.com>
2023-03-02 03:59:11 +0900

    0302

--- python/11066번.py
+++ python/11066번.py
@@ -1,26 +1,24 @@
 import sys
 input=sys.stdin.readline
 T=int(input())
-res=[]
 for _ in range(T):
     K=int(input())
-    li=list(map(int,input().split()))
-    dp=[[0]*K for _ in range(K)]
-    sums=[0]*K
-    for i in range(K-1):
+    li=[0]+list(map(int,input().split()))
+    dp=[[0]*(K+1) for _ in range(K+1)]
+    sums=[0]*(K+1)
+    for i in range(1,K):
         dp[i][i+1]=li[i]+li[i+1]
-        sums[i]=sums[i-1]+li[i] if i!=0 else li[0]
-    sums[K-1]=sums[K-2]+li[K-1]
-    for i in range(K):
-        for j in range(i+2,K):
-            
+        sums[i]=sums[i-1]+li[i]
+    sums[K]=sums[K-1]+li[K]
+    for k in range(2,K+1):
+        for i in range(1,K+1):
+            if i+k>K:
+                break
             min_v=10**9
-            for k in range(i+1,j):
-                min_v=min(min_v,dp[i][k]+dp[k][j])
-            dp[i][j]=min_v+sums[j]-sums[i-1]
-    res.append(dp[0][K-1])
-print(res)
-
+            for j in range(i,i+k):
+                min_v=min(min_v,dp[i][j]+dp[j+1][i+k])
+            dp[i][i+k]=min_v+sums[i+k]-sums[i-1]
+    print(dp[1][K])
 
 '''
 i,j,k=0,1,
