ee171b40fffb40b39256a9e2a09ee254313db767
Wjong <weon1009@gmail.com>
2023-03-08 02:11:52 +0900

    0308

---
+++ python/24313번.py
@@ -0,0 +1,8 @@
+A1,A2=map(int,input().split())
+c=int(input())
+n=int(input())
+print(1 if (A2<=(c-A1)*n)and (c-A1)>=0 else 0)
+
+'''
+-2 <= -1*n
+'''
---
+++ python/2629번_2.py
@@ -0,0 +1,27 @@
+N=int(input())
+li=[0]+list(map(int,input().split()))+[0]
+ball_N=int(input())
+ball=list(map(int,input().split()))
+dp=[[0]*(15001) for i in range(N+1)]
+
+def back(now,weight):
+    if now>N or dp[now][weight]:
+        return
+    dp[now][weight]=1
+    back(now+1,weight+li[now+1])
+    back(now+1,abs(weight-li[now+1]))
+    back(now+1,weight)
+
+back(0,0)
+for i in ball:
+    ch=0
+    if i>15000:
+        print("N",end=" ")
+    for j in range(1,N+1):
+        if dp[j][i]==1:
+            print("Y",end=" ")
+            ch=1
+            break
+    if ch==0:
+        print("N",end=" ")
+        
