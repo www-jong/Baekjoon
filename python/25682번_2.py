7358625b42b301f7930f3c02ab9a8233cc10432d
www-jong <weon1009@gmail.com>
2023-03-12 21:36:50 +0900

    0312

--- python/25682번_2.py
+++ python/25682번_2.py
@@ -2,29 +2,33 @@
 import sys
 input=sys.stdin.readline
 N,M,K=map(int,input().split())
-li=[[0]*(M+1)]
-res=0
-def check(color):
-    dp=[[0]*(M+1) for i in range(N+1)]
-    
-    for i in range(1,N+1):
-        for j in range(1,M+1):
-            if (i+j)%2==0:
-                val=li[i][j]!=color
-            else:
-                val=li[j][j]==color
-            dp[i+1][j+1]=dp[i+1][j]+dp[i][j+1]-dp[i][j]+val
-    tmp=10**9
-    for i in range(1,N-K+2):
-        for j in range(1,M-K+2):
-            tmp=min(tmp,dp[i+K-1][j+K-1]-dp[i+K-1][j-1]-dp[i-1][j+K-1]+dp[i-1][j-1])
-    return tmp
+li=[[0]]
+res=10**9
+W_board=[[0]]
+B_board=[[0]]
+
+sum_w=[[0]*(M+1) for i in range(N+1)]
+sum_b=[[0]*(M+1) for i in range(N+1)]
+
+for i in range(K):
+    W='0'+'WB'*((max(N,M)//2)+1)
+    B='0'+'BW'*((max(N,M)//2)+1)    
+    W_board.append(W if i%2==0 else B)
+    B_board.append(B if i%2==0 else W)
 for i in range(N):
-    li.append([0]+list(input()))
+    li.append('0'+input())
 
-print(check('W'))
-print(check('B'))
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        sum_w[i][j]=(1 if W_board[i][j]!=li[i][j] else 0)+sum_w[i][j-1]+sum_w[i-1][j]-sum_w[i-1][j-1]
+        sum_b[i][j]=(1 if B_board[i][j]!=li[i][j] else 0)+sum_b[i][j-1]+sum_b[i-1][j]-sum_b[i-1][j-1]
 
+for i in range(1,N-K+2):
+    for j in range(1,M-K+2):
+        W_retouch=(sum_w[i+K-1][j+K-1]-sum_w[i+K-1][j-1]-sum_w[i-1][j+K-1]+sum_w[i-1][j-1])
+        B_retouch=(sum_b[i+K-1][j+K-1]-sum_b[i+K-1][j-1]-sum_b[i-1][j+K-1]+sum_b[i-1][j-1])
+        res=min(res,W_retouch,B_retouch)
+print(res)
 '''
 세로 N
 가로 M
--- python/4134번.py
+++ python/4134번.py
@@ -1,12 +1,12 @@
+import sys
+input=sys.stdin.readline
 N=int(input())
-MAXS=4*(10**9)+1
-li=[1]*(MAXS)
-so=[]
-for i in range(2,int(MAXS**0.5)):
-    if li[i]:
-        for j in range(i+i,MAXS,i):
-            li[i]=0
 
+def check(x):
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            return False
+    return True
 
 for i in range(N):
     n=int(input())
@@ -14,7 +14,7 @@ for i in range(N):
         if n==0 or n==1:
             print(2)
             break
-        if li[n]:
+        if check(n):
             print(n)
             break
         else:
