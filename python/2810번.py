687848511a550a12360da029bbd55f4a2df9c4bd
Wjong <weon1009@gmail.com>
2023-03-18 02:12:00 +0900

    0318

--- python/2810번.py
+++ python/2810번.py
@@ -1,27 +1,7 @@
-N=int(input())
-S=list(input())
-res=0
-ch=0
-tmp='.'
-idx=0
-
-while idx<N:
-    if S[idx]=='S':
-        if tmp[-1]!="*":
-            tmp+="*"+S[idx]
-            res+=1
-        elif tmp[-1]=="*":
-            tmp+=S[idx]
-        idx+=1
-    elif S[idx]=="L":
-        if tmp[-1]!="*":
-            tmp+="*"+"LL"+"*"
-            idx+=2
-            res+=2
-        elif tmp[-1]=="*":
-            tmp+="LL"+"*"
-            idx+=2
-            res+=1
-    else:
-        idx+=1
-print(res)
+N = int(input())
+member = input()
+count = member.count('LL')
+if (count <= 1):
+    print(len(member))
+else:
+    print(len(member) - count + 1)
---
+++ python/5073번.py
@@ -0,0 +1,17 @@
+while True:
+    li=list(map(int,input().split()))
+    A,B,C=sorted(li,reverse=True)
+    print(f'A:{A} B:{B} C:{C}')
+    if A==0 and B==0 and C==0:
+        break
+    if A==B==C:
+        print('Equilateral')
+    elif A==B or B==C or A==C:
+        print('Isosceles')
+    elif A!=B and B!=C and A!=C:
+        if B+C<=A:
+            print('Invalid')
+        else:
+            print('Scalene')
+    else:
+        print('Invalid')
