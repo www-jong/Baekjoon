755836a21348640149434a42c81da7f5dc75b378
Wjong <weon1009@gmail.com>
2023-04-19 14:57:10 +0900

    0419

--- python/19944번.py
+++ python/19944번.py
@@ -2,6 +2,6 @@ N,M=map(int,input().split())
 if 1<=M<=2:
     print('NEWBIE!')
 elif M<=N:
-    print('TLE!')
-else:
     print("OLDBIE!")
+else:
+    print('TLE!')
---
+++ python/23795번.py
@@ -0,0 +1,7 @@
+res=0
+while True:
+    s=int(input())
+    if s==-1:
+        break
+    res+=s
+print(res)
