f3dc6e03216ca76e4dd9c60cec0bf4f38184933e
www-jong <weon1009@gmail.com>
2023-06-01 17:30:18 +0900

    0601

--- python/15953번.py
+++ python/15953번.py
@@ -1,11 +1,10 @@
-T=int(input())
-for i in range(T):
-    s=input()
+ali=[0]+[500]+[300]*2+[200]*3+[50]*4+[30]*5+[10]*6
+bli=[0]+[512]+[256]*2+[128]*4+[64]*8+[32]*16
+for i in range(int(input())):
     res=0
-    for i in range(26):
-        if len(s)>i:
-            if ord(s[i])!=i+65:
-                res+=i+65
-        else:
-            res+=i+65
-    print(res)
+    a,b=map(int,input().split())
+    if a<=21:
+        res+=ali[a]
+    if b<=31:
+        res+=bli[b]
+    print(res*10000)
