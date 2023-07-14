7fcb013d6587b57b91e4aea8f2d4532d11fafb6c
www-jong <weon1009@gmail.com>
2023-07-14 14:29:43 +0900

    0714

--- python/1644번.py
+++ python/1644번.py
@@ -7,11 +7,29 @@ for i in range(2,N+1):
         primes.append(i)
         for j in range(2*i,N+1,i):
             check[j]=0
-
-st=0
-end=0
-sums=0
-while primes[end]<N:
-    
-
-print(primes)
+if N!=1:
+    st=0
+    end=0
+    sums=primes[st]
+    res=0
+    while end<len(primes):
+        if sums==N:
+            res+=1
+            sums-=primes[st]
+            st+=1
+        elif sums<N:
+            if end<len(primes)-1:
+                end+=1
+                sums+=primes[end]
+            else:
+                break
+        else:
+            if st==end:
+                break
+            sums-=primes[st]
+            st+=1
+        #print(f'now : {st}:{end} --> {sums}')
+    #print(primes)
+    print(res)
+else:
+    print(0)
