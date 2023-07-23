978b8ae9e5bf0b4c72dd0e719171e77b58901826
www-jong <weon1009@gmail.com>
2023-07-23 18:42:31 +0900

    0723

--- python/17070번.py
+++ python/17070번.py
@@ -1,13 +1,5 @@
 N=int(input())
-
-def right(): # 오른쪽
-    pass
-
-def down(): # 아래
-    pass
-def diag(): # 대각선
-    pass
-
-li=[[]]
-for _ in range(N):
-    li.append([0]+list(map(int,input().split())))
+li=[]
+for i in range(N):
+    li.append(list(map(int,input().split())))
+graph=[]
---
+++ python/26575번 copy.py
@@ -0,0 +1,3 @@
+for i in range(int(input())):
+    A,B,C=map(float,input().split())
+    print("$%.2f"%(B*C*A))
