f3e108fc9134d20d28e711464ebdea31270ff8fb
www-jong <weon1009@gmail.com>
2023-08-15 21:50:06 +0900

    0815

--- python/1967번.py
+++ python/1967번.py
@@ -12,27 +12,25 @@ sys.setrecursionlimit(100000)
 input=sys.stdin.readline
 def func(x):
     global res
-    left_val,right_val=0,0
-    if left_tree[x][0]!=0:
-        left_val=func(left_tree[x][0])+left_tree[x][1]
-    if right_tree[x][0]!=0:
-        right_val=func(right_tree[x][0])+right_tree[x][1]
-    #print(x,left_val,right_val)
-    res=max(res,left_val,right_val,left_val+right_val)
-    return max(left_val,right_val)
-
+    tmp_vals=[]
+    for idx,v in tree[x]:
+        tmp_vals.append(func(idx)+v)
+    tmp_vals.sort(reverse=True)
+    if tmp_vals:
+        res=max(res,tmp_vals[0]+(tmp_vals[1] if len(tmp_vals)>=2 else 0))
+        return tmp_vals[0]
+    else:
+        return 0
 n=int(input())
 check=[0]*(n+1)
 check[0]=1
-left_tree=[[0,0] for i in range(n+1)]
-right_tree=[[0,0] for i in range(n+1)]
+
+tree=[[] for i in range(n+1)]
 for i in range(n-1):
     a,b,c=map(int,input().split())
     check[b]=1
-    if left_tree[a][0]==0:
-        left_tree[a]=[b,c]
-    else:
-        right_tree[a]=[b,c]
+    tree[a].append([b,c])
+
 res=0
 for i in range(1,n+1):
     if check[i]==0:
