d62cd1a9c10ec73a24be849c238d10ea995f5988
Wjong <weon1009@gmail.com>
2023-09-28 20:14:24 +0900

    0928

---
+++ python/29738번.py
@@ -0,0 +1,23 @@
+T=int(input())
+for i in range(1,T+1):
+    N=int(input())
+    if N<=25:
+        print(f'Case #{i}: World Finals')    
+    elif N<=1000:
+        print(f'Case #{i}: ROUND 3')
+    elif N<=4500:
+        print(f'Case #{i}: ROUND 2')
+    else:
+        print(f'Case #{i}: ROUND 1')
+
+T = int(input())
+for i in range(1, T+1):
+    N = int(input())
+    if N > 4500:
+        print(f'Case #{i}: Round 1')
+    elif N > 1000:
+        print(f'Case #{i}: Round 2')
+    elif N > 25:
+        print(f'Case #{i}: Round 3')
+    else:
+        print(f'Case #{i}: World Finals')
