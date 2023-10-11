35e067c693b28efc92742ac0cd186aa58deefde1
weon1009 <weon1009@gmail.com>
2023-10-11 17:42:08 +0900

    1011

--- java/2587번.java
+++ java/2587번.java
@@ -5,18 +5,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.lang.Math;
-class test {
+import java.util.Arrays;
+class Main {
 	static public void main(String []args) throws IOException{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
+        int li[] = new int[5];
         int res = 0;
-        System.out.println(N/5);
-        System.out.println((int)N/5);
-        
-        bw.write(res);
+        for(int i=0;i<5;i++){
+            int tmp = Integer.parseInt(br.readLine());
+            res+=tmp;
+            li[i]=tmp;
+            }
+        Arrays.sort(li);
+        bw.write(Integer.toString(res/5)+'\n'+Integer.toString(li[2]));
         bw.flush();
         bw.close();
 
--- java/2750번.java
+++ java/2750번.java
@@ -5,18 +5,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.lang.Math;
-class test {
+import java.util.Arrays;
+class Main {
 	static public void main(String []args) throws IOException{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int res = 0;
-        System.out.println(N/5);
-        System.out.println((int)N/5);
-        
-        bw.write(res);
+        int li[] = new int[N];
+        Arrays.fill(li,1);
+        for(int i=0;i<N;i++){
+            li[i]=Integer.parseInt(br.readLine());
+            }
+        Arrays.sort(li);
+        for(int i=0;i<N;i++){
+            bw.write(Integer.toString(li[i])+'\n');
+        }
         bw.flush();
         bw.close();
 
