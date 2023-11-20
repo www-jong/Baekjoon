c1613184be48d28d3cbeb05cf2c57096b4a7b4a3
www-jong <weon1009@gmail.com>
2023-11-20 21:53:03 +0900

    1120

---
+++ python/30676번.py
@@ -0,0 +1,17 @@
+n=int(input())
+s=''
+if 620<=n<=780:
+    s='Red'
+elif 590<=n:
+    s='Orange'
+elif 570<=n:
+    s='Yellow'
+elif 495<=n:
+    s='Green'
+elif 450<=n:
+    s='Blue'
+elif 425<=n:
+    s='Indigo'
+elif 380<=n:
+    s='Violet'
+print(s)
