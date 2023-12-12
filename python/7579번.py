1a054f5d557b3bf425d9adf65aeb45588e0a9740
www-jong <weon1009@gmail.com>
2023-12-12 18:40:06 +0900

    1212

--- python/7579번.py
+++ python/7579번.py
@@ -1,14 +1,17 @@
-n,m=map(int,input().split())
+import sys
+input=sys.stdin.readline
+INF=float('inf')
+N,M=map(int,input().split())
 A=list(map(int,input().split()))
 C=list(map(int,input().split()))
-dp=[[0 for _ in range(sum(C)+1)] for _ in range(n+1)]
-res=sum(C)
-for i in range(1,n):
-    for j in range(1,sum(C)+1):
+res=INF
+dp=[[0]*(sum(C)+1) for _ in range(N)]
+for i in range(N):
+    for j in range(sum(C)+1):
         if j<C[i]:
             dp[i][j]=dp[i-1][j]
         else:
-            dp[i][j]=max(A[i]+dp[i-1][j-C[i]],dp[i-1][j])
-        if dp[i][j]>=m:
-            res=min(res,dp[i][j])
+            dp[i][j]=max(dp[i-1][j-C[i]]+A[i],dp[i-1][j])
+        if dp[i][j]>=M:
+            res=min(res,j)
 print(res)
