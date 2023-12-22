ad2e065f3662eb6aae5acdd78a23684beb37a013
www-jong <weon1009@gmail.com>
2023-12-22 19:00:25 +0900

    1222

---
+++ python/13416번.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    mymax=0
+    for i in range(int(input())):
+        tmp=list(map(int, input().split()))
+        if max(tmp)>=0: mymax+=max(tmp)
+    print(mymax)
