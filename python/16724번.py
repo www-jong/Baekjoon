80cbcd99128a892fef58a39ff91b579e4753a049
www-jong <weon1009@gmail.com>
2024-01-08 23:03:01 +0900

    0108

--- python/16724번.py
+++ python/16724번.py
@@ -1,29 +1,33 @@
+from collections import deque
+d={'U':[-1,0],'D':[1,0],'L':[0,-1],'R':[0,1]}
 N,M=map(int,input().split())
-li=[[0]]
-for i in range(N):
-    li.append('0'+input())
-dic={'D':[1,0],'U':[-1,0],'R':[0,1],'L':[0,-1]}
+li=[input() for _ in range(N)]
+visit=[[0]*(M) for _ in range(N)]
 res=0
-visit=[[0]*(M+1) for i in range(N+1)]
-ch={}
-def dfs(x,y,val):
-    global res,visit,ch
-    visit[x][y]=val
 
-    nx,ny=dic[li[x][y]][0]+x,dic[li[x][y]][1]+y
-    if visit[nx][ny]==0:
-        ch[str(nx)+' '+str(ny)]=1
-        dfs(nx,ny,val)
-    else:
-        return [nx,ny]
-
-
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        if visit[i][j]==0:
-            ch[str(i)+' '+str(j)]=1
-            s=dfs(i,j,1)
-            if s:
-                if str(s[0])+' '+str(s[1]) not in ch:
+for i in range(N):
+    for j in range(M):
+        tmp={}
+        q=deque([])
+        if not visit[i][j]:
+            tmp[(i,j)]=1
+            q.append((i,j))
+            while q:
+                x,y=q.popleft()
+                nx,ny=x+d[li[x][y]][0],y+d[li[x][y]][1]
+                if 0<=nx<N and 0<=ny<M:
+                    if (nx,ny) in tmp:
+                        res+=1
+                        break
+                    elif visit[nx][ny]:
+                        break
+                    else:
+                        tmp[(nx,ny)]=1
+                        q.append((nx,ny))
+                else:
                     res+=1
+                    break
+            for sx,sy in tmp.keys():
+                visit[sx][sy]=1
+
 print(res)
