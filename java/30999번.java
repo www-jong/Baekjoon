2bb76ba43471c1ee45bcc42fd0a75bb519641332
www-jong <weon1009@gmail.com>
2024-01-28 19:47:39 +0900

    0128

--- java/30999번.java
+++ java/30999번.java
@@ -3,22 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class test {
+public class Main {
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        List<String> lines = new ArrayList<String>();
+        int res = 0;
         for(int i=0; i<N;i++){
-            lines.add(br.readLine());
+            int tmp =0;
+            String line = br.readLine();
+            for(int j=0;j<line.length();j++){
+                if(line.charAt(j)=='O'){tmp++;}
+            }
+            if(tmp>=Math.ceil(M/2.0)){res++;}
         }
-        System.out.println(lines);
+        System.out.println(res);
 
     }
 
     public static void main(String[] args) throws IOException {
-        new test().solution();
+        new Main().solution();
     }
 }
--- java/temp.java
+++ java/temp.java
@@ -1,22 +1,16 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class test {
-	static public void main(String []args) throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+
+    public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        String res = "0";
-
-        bw.write(res);
-        bw.flush();
-        bw.close();
-
+    }
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
     }
 }
