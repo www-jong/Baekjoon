21347c7830df35b8000b379272a454708262fff8
www-jong <weon1009@gmail.com>
2024-01-30 23:23:02 +0900

    0130

--- java/30468번.java
+++ java/30468번.java
@@ -7,8 +7,16 @@ public class Main {
 
     public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int res = 0 ;
+        int stat = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        for(int i=0;i<4;i++){
+            stat+=Integer.parseInt(st.nextToken());
+        }
+        int n=Integer.parseInt(st.nextToken());
+        res=Math.max(0,4*n-stat);
+        System.out.println(res);
+        
     }
     public static void main(String[] args) throws Exception {
         new Main().solution();
--- java/31090번.java
+++ java/31090번.java
@@ -8,7 +8,16 @@ public class Main {
     public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        int n = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++){
+            String now = br.readLine();
+            if((Integer.parseInt(now)+1)%Integer.parseInt(now.substring(2,4))!=0){
+                System.out.println("Bye");
+            }else{
+                System.out.println("Good");
+            }
+        }
+        
     }
     public static void main(String[] args) throws Exception {
         new Main().solution();
